2a6cf94a3344c7d87df889926d09bd529adf49d9
Anton <a.chertok@akatech.com.ua>
2024-02-02 18:25:54 +0000

    storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,19 +6,38 @@ import Section from 'components/Section';
 import { Component } from 'react';
 import { filterList } from 'utils/filterList';
 import { MainHeading } from './App.styled';
+import Storage from 'services/storage';
+
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', tel: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', tel: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', tel: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', tel: '227-91-26' },
+];
 
 class App extends Component {
-  state = {
-    // contacts: [],
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', tel: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', tel: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', tel: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', tel: '227-91-26' },
-    ],
+  constructor() {
+    super();
+    this.storage = new Storage('contacts');
+  }
 
+  state = {
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    this.setState(prevState => ({
+      ...prevState,
+      contacts: this.storage.get() || initialContacts,
+    }));
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.state.contacts !== prevState.contacts) {
+      this.storage.set(this.state.contacts);
+    }
+  }
 
   addContact = contact => {
     const hasContact = this.state.contacts.some(
